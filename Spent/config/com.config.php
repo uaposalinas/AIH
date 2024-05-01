ccb33f64e77603fd7e15f720aafda4a0bc3734b5
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-05-01 16:25:48 -0600

    Add files via upload

---
+++ Spent/config/com.config.php
@@ -0,0 +1,10 @@
+<?php
+
+    $UserName = "root";
+    $ServerName = "localhost";
+    $Password = "";
+    $DatabaseName = "aihspends";
+
+    $Connection = new mysqli($ServerName, $UserName, $Password, $DatabaseName);
+
+?>
