aa89915457d3c4d7bfd1a51deb8931bc07cee395
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-05-06 00:46:38 -0600

    none

--- Spent/SetLog/index.php
+++ Spent/SetLog/index.php
@@ -4,6 +4,10 @@
 
     $Connection = new mysqli($ServerName, $UserName, $Password, $DatabaseName);
 
+
+    $Connection->set_charset("utf8");
+
+
     if($Connection){
 
         if(isset($_POST["FullDate"]) && isset($_POST["CurrentDay"]) && isset($_POST["GestID"]) && isset($_POST["Month"]) && isset($_POST["Year"]) && isset($_POST["SendProvider"]) && isset($_POST["SendAmount"]) && isset($_POST["SendDescription"]) && isset($_POST["SendCountableCount"]) && isset($_POST["SendBuyType"])  && isset($_POST["SendPayType"]) && isset($_POST["Realice"])  && isset($_POST["SendDate"])  && isset($_POST["SendBillID"]) && isset($_POST["SendSubtotal"]) && isset($_POST["SendExempt"]) && isset($_POST["Other"]) && isset($_POST["ISV18"]) && isset($_POST["ISV15"]) && isset($_POST["SendTotal"]) && isset($_POST["CardUsed"])){
