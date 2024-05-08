db60f4558d02649243243676e33663a58b5a2552
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-05-08 12:16:01 -0600

    none

--- Spent/config/com.config.php
+++ Spent/config/com.config.php
@@ -1,10 +1,16 @@
 <?php
 
-    $UserName = "root";
-    $ServerName = "localhost";
-    $Password = "";
-    $DatabaseName = "aihspends";
+   # $UserName = "root";
+   # $ServerName = "localhost";
+   # $Password = "";
+   # $DatabaseName = "aihspends";
 
-    $Connection = new mysqli($ServerName, $UserName, $Password, $DatabaseName);
+   $UserName = "devlabsc_root";
+   $ServerName = "sv18.byethost18.org";
+   $Password = "Dv229011000";
+   $DatabaseName = "devlabsc_aihspends";
+
+
+   $Connection = new mysqli($ServerName, $UserName, $Password, $DatabaseName);
 
 ?>
