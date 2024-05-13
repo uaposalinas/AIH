9037c256a8a384881195f3ad245900a865a77841
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-05-13 16:40:58 -0600

    none

--- Spent/Vendor/com.js/com.versions.js
+++ Spent/Vendor/com.js/com.versions.js
@@ -1,4 +1,5 @@
 const Scripts = document.getElementsByTagName("script");
+const Links = document.getElementsByTagName('link')
 
 for(let Aument = 0; Aument < Scripts.length; Aument++){
 
@@ -8,5 +9,16 @@ for(let Aument = 0; Aument < Scripts.length; Aument++){
     const NewLocation = `${GetLocation}?v=${Math.random()}`;
 
     AllScripts.src = NewLocation;
+
+}
+
+for(let Aument = 0; Aument < Links.length; Aument++){
+
+    const AllLinks = Links[Aument];
+    const GetLocation = Links.getAttribute("href");
+
+    const NewLocation = `${GetLocation}?v=${Math.random()}`;
+
+    AllLinks.href = NewLocation;
 
 }
