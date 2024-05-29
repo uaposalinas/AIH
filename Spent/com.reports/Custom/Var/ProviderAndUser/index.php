241599582322fc76ec8427c6f44f8a9cd731eaa2
devlabscorporation <121150895+devlabscorporation@users.noreply.github.com>
2024-05-29 17:21:32 -0600

    none

--- Spent/com.reports/Custom/Var/ProviderAndUser/index.php
+++ Spent/com.reports/Custom/Var/ProviderAndUser/index.php
@@ -4,37 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ver reporte mes de Mayo</title>
-    <link rel="shortcut icon" href="Assets/com.img/com.icon.png" type="image/x-icon">
-    <link rel="stylesheet" href="../Vendor/com.css/com.config.css">
-    <link rel="stylesheet" href="../Vendor/com.css/com.reports.print.css">
-    <link rel="stylesheet" href="../Fonts/IndexFontsCaviarDreams.css">
-    <link rel="stylesheet" href="../Fonts/IndexFontsRoboto.css">
+    <link rel="shortcut icon" href="../../../../Assets/com.img/com.icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="../../../../Vendor/com.css/com.config.css">
+    <link rel="stylesheet" href="../../../../Vendor/com.css/com.reports.print.css">
+    <link rel="stylesheet" href="../../../../Fonts/IndexFontsCaviarDreams.css">
+    <link rel="stylesheet" href="../../../../Fonts/IndexFontsRoboto.css">
 </head>
 <body class="ReportsBody Scroll">
 
-    <header>
+<header>
 
-        <div class="HeaderDecoration"></div>
+<div class="HeaderDecoration"></div>
 
-            <div class="QrCode"></div>
+    <div class="QrCode"></div>
 
 
-        <div class="LeftZoneHeader">
+<div class="LeftZoneHeader">
 
-            <t>Reporte Mensual de Gastos</t>
-            <p>Mayo 2024</p>
+    <t>Reporte Personalizado de Gastos</t>
+    <p>Proveedor y Comprador</p>
 
-            <div class="ReportDetails">
+    <div class="ReportDetails">
 
-                <t2>Detalles del reporte</t2>
-                <p2> <b>Fecha de cierre de mes:</b> 31 de Mayo de 2024</p2>
-                <p2> <b>ID de reporte:</b> ASC-2024-041001MR</p2>
-                <p2> <b>Generado por:</b> Alejandro Salinas</p2>
-            </div>
+        <t2>Detalles del reporte</t2>
+        <p2> <b>Realizado el:</b> 31 de Mayo de 2024</p2>
+        <p2> <b>ID de reporte:</b> ASC-2024-041001MR</p2>
+        <p2> <b>Generado por:</b> Alejandro Salinas</p2>
+    </div>
 
-        </div>
+</div>
 
-    </header>
+</header>
 
 
     <div class="ShowReportsPerMonth">
@@ -69,20 +69,22 @@
 
                     $Number = 0;
 
-                    require '../config/com.config.php';
+                    require '../../../../config/com.config.php';
                     $Connection -> set_charset("utf8");
 
-                    if(isset($_GET["MonthID"])){
-
-                        $Month = $_GET["MonthID"];
-
+                    if(isset($_GET["User"]) && isset($_GET["Provider"])){
+                
+                        $User = $_GET["User"];
+                        $Provider = $_GET["Provider"];
+                
                     }else{
-
-                        echo "<script> window.location.href = '../' </script>";
-
+                
+                        ////echo "<script> window.location.href = '../' </script>";
+                
                     }
+                
 
-                    $DoQuery = "SELECT * FROM logs WHERE Month = '$Month'";
+                    $DoQuery = "SELECT * FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
                     $QueryResults = $Connection -> query($DoQuery);
 
                     if($QueryResults -> num_rows > 0){
@@ -183,7 +185,7 @@
 
                     }else{
                         
-                        echo "<script> try {window.close(); localStorage.setItem('NoLogKey', 'true') } catch (error) {window.location.href = '../';} </script>";
+                        //echo "<script> try {window.close(); localStorage.setItem('NoLogKey', 'true') } catch (error) {window.location.href = '../../../../';} </script>";
 
 
                     }
@@ -220,20 +222,22 @@
 
         <?php
 
-            require '../config/com.config.php';
+            require '../../../../config/com.config.php';
 
-            if(isset($_GET["MonthID"])){
-
-                $Month = $_GET["MonthID"];
-
+            if(isset($_GET["From"]) && isset($_GET["Until"]) && isset($_GET["Provider"])){
+                
+                $From = $_GET["From"];
+                $User = $_GET["Until"];
+                $Provider = $_GET["Provider"];
+        
             }else{
-
-                echo "<script> window.location.href = '../' </script>";
-
+        
+                ////echo "<script> window.location.href = '../' </script>";
+        
             }
 
 
-            $DoQuery = "SELECT Exempt FROM logs WHERE Month = '$Month'";
+            $DoQuery = "SELECT Exempt FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
 
             $QueryResults = $Connection->query($DoQuery);
 
@@ -265,20 +269,22 @@
 
             <?php
 
-                require '../config/com.config.php';
+                require '../../../../config/com.config.php';
 
-                if(isset($_GET["MonthID"])){
-
-                    $Month = $_GET["MonthID"];
-
+                if(isset($_GET["From"]) && isset($_GET["Until"]) && isset($_GET["Provider"])){
+                
+                    $From = $_GET["From"];
+                    $User = $_GET["Until"];
+                    $Provider = $_GET["Provider"];
+            
                 }else{
-
-                    echo "<script> window.location.href = '../' </script>";
-
+            
+                    ////echo "<script> window.location.href = '../' </script>";
+            
                 }
 
 
-                $DoQuery = "SELECT ISV15 FROM logs WHERE Month = '$Month'";
+                $DoQuery = "SELECT ISV15 FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
 
                 $QueryResults = $Connection->query($DoQuery);
 
@@ -310,20 +316,21 @@
 
                 <?php
 
-                    require '../config/com.config.php';
+                    require '../../../../config/com.config.php';
 
-                    if(isset($_GET["MonthID"])){
-
-                        $Month = $_GET["MonthID"];
-
+                    if(isset($_GET["From"]) && isset($_GET["Until"]) && isset($_GET["Provider"])){
+                
+                        $From = $_GET["From"];
+                        $User = $_GET["Until"];
+                        $Provider = $_GET["Provider"];
+                
                     }else{
-
-                        echo "<script> window.location.href = '../' </script>";
-
+                
+                        ////echo "<script> window.location.href = '../' </script>";
+                
                     }
 
-
-                    $DoQuery = "SELECT ISV18 FROM logs WHERE Month = '$Month'";
+                    $DoQuery = "SELECT ISV18 FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
 
                     $QueryResults = $Connection->query($DoQuery);
 
@@ -354,20 +361,22 @@
 
             <?php
 
-                require '../config/com.config.php';
+                require '../../../../config/com.config.php';
 
-                if(isset($_GET["MonthID"])){
-
-                    $Month = $_GET["MonthID"];
-
+                if(isset($_GET["From"]) && isset($_GET["Until"]) && isset($_GET["Provider"])){
+                
+                    $From = $_GET["From"];
+                    $User = $_GET["Until"];
+                    $Provider = $_GET["Provider"];
+            
                 }else{
-
-                    echo "<script> window.location.href = '../' </script>";
-
+            
+                    ////echo "<script> window.location.href = '../' </script>";
+            
                 }
 
 
-                $DoQuery = "SELECT OtherISV FROM logs WHERE Month = '$Month'";
+                $DoQuery = "SELECT OtherISV FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
 
                 $QueryResults = $Connection->query($DoQuery);
 
@@ -396,20 +405,22 @@
 
             <?php
 
-            require '../config/com.config.php';
+            require '../../../../config/com.config.php';
 
-            if(isset($_GET["MonthID"])){
-
-                $Month = $_GET["MonthID"];
-
+            if(isset($_GET["From"]) && isset($_GET["Until"]) && isset($_GET["Provider"])){
+                
+                $From = $_GET["From"];
+                $User = $_GET["Until"];
+                $Provider = $_GET["Provider"];
+        
             }else{
-
-                echo "<script> window.location.href = '../' </script>";
-
+        
+                ////echo "<script> window.location.href = '../' </script>";
+        
             }
 
 
-            $DoQuery = "SELECT Total FROM logs WHERE Month = '$Month'";
+            $DoQuery = "SELECT Total FROM logs WHERE SpendedBy = '$User' AND Provider = '$Provider';";
 
             $QueryResults = $Connection->query($DoQuery);
 
@@ -438,237 +449,10 @@
                     
     </div>
 
-    <div class="ShowReportsPerMonth ShowExempts">
 
-        <t class="PerMonthsLogTitle" style="width: 10px;">Registros exentos del mes de Mayo</t>
-        <div class="TitleBorder"></div>
-
-        <div class="Table" style="position:relative; left:calc(50% - 37%)">
-
-            <div class="Identifers">
-
-                
-                <columns>NO.</columns>
-                <columns>Fecha</columns>
-                <columns style="137.4px !important">No. de Factura</columns>
-                <columns style="width:164.36px">Nombre de Proveedor</columns>
-                <columns>Cnt.</columns>
-                <columns>Cuenta Cont.</columns>
-                <columns>Subtotal</columns>
-                <columns>Exento</columns>
-                <columns>Total de Gasto</columns>
-                <columns>Pago</columns>
-                <columns>Fin</columns>
-            </div>
-
-            <div class="Logs">
-                
-                <?php
-
-                    $Number = 0;
-
-                    require '../config/com.config.php';
-                    $Connection -> set_charset("utf8");
-
-                    if(isset($_GET["MonthID"])){
-
-                        $Month = $_GET["MonthID"];
-
-                    }else{
-
-                    echo "<script> try {window.close();} catch (error) {window.location.href = '../';} </script>";
-
-
-                    }
-
-
-                    $DoQuery = "SELECT * FROM logs WHERE Month = '$Month'";
-                    $QueryResults = $Connection -> query($DoQuery);
-
-                    if($QueryResults -> num_rows > 0){
-
-                        while($Row = $QueryResults -> fetch_assoc()){
-
-                            $Provider = $Row["Provider"];
-                            $Date = $Row["Date"];
-                            $Amount = $Row["Amount"];
-                            $PayType = $Row["PayType"];
-                            $BillNumber = $Row["BillNumber"];
-                            $CountableCount = $Row["CountableCount"];
-                            $BuyType = $Row["BuyType"];
-                            $Subtotal = $Row["Subtotal"];
-                            $Exempt = $Row["Exempt"];
-                            $OtherISV = $Row["OtherISV"];
-                            $ISV15 = $Row["ISV15"];
-                            $ISV18 = $Row["ISV18"];
-                            $Total = $Row["Total"];
-
-                            //FormattedValues 
-
-                            if($PayType == "Efectivo"){
-
-                                $PayType = "Efc";
-
-                            }else if($PayType == "Transferencia"){
-
-                                $PayType = "Trans.";
-
-                            }else if($PayType == "Tarjeta de Crédito"){
-
-                                $PayType = "T/C";
-
-                            }else if($PayType == "Pago en línea"){
-
-                                $PayType = "Online";
-
-                            }else if($PayType == "Botón de Pago"){
-
-                                $PayType = "BDP";
-
-                            }
-                            
-
-                            if($BuyType == "Personal"){
-
-                                $BuyType = "Prs";
-
-                            }else if($BuyType == "Oficina"){
-
-                                $BuyType = "Ofc";
-
-                            }
-
-                            $AmountInt = floatval($Amount);
-
-                            if($Amount < 10){
-
-                                $Amount = '0'. $Amount;
-
-                            }
-
-                            if ($OtherISV === '') {
-                                $OtherISV = "L 0.00";
-                            }
-                            
-
-                            if($Exempt == "0.00"){
-
-                             
-
-                            }else{
-
-                                $Number++;
-
-                                echo "
-
-                                <div class='ThisRes ThisResExempts'>
-
-                                <divs><n>$Number</n></divs>
-                                <divs><p class='MountDate'>$Date</p></divs>
-                                <divs><p>$BillNumber</p></divs>
-                                <divs><p>$Provider</p></divs>           
-                                <divs><p>$Amount</p></divs>
-                                <divs><p>$CountableCount</p></divs>
-                                <divs><p class='Subtotals'>$Subtotal</p></divs>
-                                <divs><p class='ExemptsNow'>$Exempt</p></divs>
-                                <divs><p class='Totals GetExentTotals'>$Exempt</p></divs>
-                                <divs><p>$PayType</p></divs>
-                                <divs><p>$BuyType</p></divs>    
-                                
-                             
-                                
-            
-                            </div>
-                            
-                            ";
-
-                            }
-
-
-
-                        }
-
-                    }else{
-                        
-                        echo "try {window.close();} catch (error) {window.location.href = '../';}";
-
-                    }
-
-                ?>
-                   
-            </div>
-
-        </div>
-
-    </div>
     
     <div class="ShowAllResults">
 
-    
-<t class="PerMonthsLogTitle" style="width: 10000px;">Totalizaciones exentos mes de mayo</t>
-   <div class="TitleBorder2"></div>
-
-   <div class="Totalizate" style="width:300px; top:70px; left:20px;">
-
-       <div class="Identifers">
-
-               <columns>Exentos</columns>
-               <columns>Total de Registros</columns>
-
-       </div>
-
-   <div class="Scapes">
-
-   <!--Exentos-->
-
-   <?php
-
-       require '../config/com.config.php';
-
-       if(isset($_GET["MonthID"])){
-
-        $Month = $_GET["MonthID"];
-
-    }else{
-
-        echo "<script> window.location.href = '../' </script>";
-
-    }
-
-
-       $DoQuery = "SELECT Exempt FROM logs WHERE Month = '$Month'";
-
-       $QueryResults = $Connection->query($DoQuery);
-
-       $ExemptAdd = 0;
-
-       if ($QueryResults->num_rows > 0) {
-           
-           while($row = $QueryResults->fetch_assoc()) {
-           
-               $ExemptAdd += floatval($row["Exempt"]);
-           }
-           
-           echo "<res class='ScapeExempts'>$ExemptAdd</res>";
-           echo "<res class='ScapeExemptS'>$ExemptAdd</res>";
-
-       } else {
-
-           echo "<res class='ScapeExempts'>$ExemptAdd</res>";
-
-       }
-
-       $Connection->close();
-
-       ?>
-
-       <!--Exentos-->
-
-   </div>
-
-   </div>
-               
-</div>
     <footer style="display: none;">
 
         <div class="FooterDecoration"></div>    
