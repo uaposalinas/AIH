4a4b355dd6aa2b056f148132299fbffc383581cf
devlabscorporation <121150895+uaposalinas@users.noreply.github.com>
2024-06-13 09:58:39 -0600

    none

--- Backups/Spent/index.php
+++ Backups/Spent/index.php
@@ -6,6 +6,9 @@ $dbname = "aihspends";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+$conn->set_charset("utf8");
+
+
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
