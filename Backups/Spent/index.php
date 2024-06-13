dc045a7794e89dcb5b9962d2f3304e22a9b3d1ac
devlabscorporation <121150895+uaposalinas@users.noreply.github.com>
2024-06-13 13:45:14 -0600

    none

--- Backups/Spent/index.php
+++ Backups/Spent/index.php
@@ -107,7 +107,50 @@ function getNextBackupVersion($backup_date_folder) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>com.backup.spent</title>
 </head>
+
+    <style>
+
+.spinner {
+  position: relative;
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+
+.spinner::before,
+.spinner::after {
+  content: "";
+  position: absolute;
+  border-radius: inherit;
+}
+
+.spinner::before {
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(0deg, #ff00cc 0%, #212121 50%);
+  animation: spin .8s infinite linear;
+}
+
+.spinner::after {
+  width: 85%;
+  height: 85%;
+  background-color: #212121;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+    </style>
+
 <body>
+
+<div class="spinner"></div>
     
     <script> 
 
