b27d2fc9a91eb4a32c606c50528e7da12d3d8c9d
devlabscorporation <121150895+uaposalinas@users.noreply.github.com>
2024-06-29 10:53:09 -0600

    none

--- Buys/com.reports/index.php
+++ Buys/com.reports/index.php
@@ -78,7 +78,7 @@
 
                     }else{
 
-                        echo "<script> window.location.href = '../' </script>";
+                      //  echo "<script> window.location.href = '../' </script>";
 
                     }
 
@@ -273,7 +273,7 @@
 
                 }else{
 
-                    echo "<script> window.location.href = '../' </script>";
+                  //  echo "<script> window.location.href = '../' </script>";
 
                 }
 
@@ -318,7 +318,7 @@
 
                     }else{
 
-                        echo "<script> window.location.href = '../' </script>";
+                      //  echo "<script> window.location.href = '../' </script>";
 
                     }
 
@@ -362,7 +362,7 @@
 
                 }else{
 
-                    echo "<script> window.location.href = '../' </script>";
+                  //  echo "<script> window.location.href = '../' </script>";
 
                 }
 
